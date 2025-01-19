00753cbed7c99b93b36d31be368f6e72331588a6
carth501 <carth501@gmail.com>
2025-01-19 17:47:26 -0500

    create claws weapon

--- scripts/player.gd
+++ scripts/player.gd
@@ -3,6 +3,7 @@ class_name Player extends CharacterBody2D
 @export var stats: Stats
 @export var speed = 400
 @export var healthbar:= ProgressBar
+@export var weapon: Node 
 
 func get_input():
 	var input_direction = Input.get_vector("left", "right", "up", "down")
