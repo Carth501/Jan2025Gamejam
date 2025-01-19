1639697aca4360b19de1cfc7557f29871b466093
Carth501 <carth501@gmail.com>
2025-01-19 13:05:18 -0500

    Implement enemy movement (#13)

--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -1,3 +1,18 @@
-class_name Enemy extends RigidBody2D
+class_name Enemy extends CharacterBody2D
 
 @export var stats:Stats
+@export var SPEED := 200.0
+var player: Player
+
+func set_player(new_player: Player):
+	player = new_player
+
+func _process(delta: float) -> void:
+	move_and_slide()
+	if(player != null):
+		var vector_to = player.position - position
+		if(vector_to.length() < 20):
+			velocity = Vector2.ZERO
+		else:
+			velocity = vector_to.normalized() * SPEED
+	
