3403c4509db3395d0051dcc3c0457e2001a0c35f
Carth501 <carth501@gmail.com>
2025-01-18 20:00:40 -0500

    Spawning enemies (#11)

--- scripts/level_.gd
+++ scripts/level_.gd
@@ -1,21 +1,26 @@
 class_name Level extends Node2D
 
 @export var player:Player
+var enemies: Array[Enemy]
+@onready var enemy_prefab = preload("res://scenes/enemy.tscn")
 @export var doors:Array[Door]
 
 var data:LevelDataHandoff
 
 func _ready() -> void:
-	player.disable()
-	player.visible = false
 	# if we aren't transition between levels, we don't need to wait for the SceneManager to call this
 	if data == null:
 		enter_level()
+	var timer = Timer.new()
+	add_child(timer)
+	timer.wait_time = 1.0
+	timer.one_shot = false
+	timer.timeout.connect(spawn_enemy)
+	timer.start()
 
 func enter_level() -> void:
 	if data != null:
 		init_player_location()
-	player.enable()
 	_connect_to_doors()
 
 # put player in front of the correct door, facing the correct direction
@@ -48,3 +53,8 @@ func _disconnect_from_doors() -> void:
 	for door in doors:
 		if door.player_entered_door.is_connected(_on_player_entered_door):
 			door.player_entered_door.disconnect(_on_player_entered_door)
+
+func spawn_enemy() -> void:
+	print("spawn_enemy")
+	var new_enemy = enemy_prefab.instantiate()
+	add_child(new_enemy)
