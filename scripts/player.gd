c10a9fb348bf73c74c69d9780aa054e75f47bbe5
Carth501 <carth501@gmail.com>
2025-01-19 20:27:41 -0500

    implement pivot for player weapons (#18)

--- scripts/player.gd
+++ scripts/player.gd
@@ -3,11 +3,14 @@ class_name Player extends CharacterBody2D
 @export var stats: Stats
 @export var speed = 400
 @export var healthbar:= ProgressBar
-@export var weapon: Node 
+@export var weapon: Node
+@export var pivot: Node2D 
 
 func get_input():
 	var input_direction = Input.get_vector("left", "right", "up", "down")
 	velocity = input_direction * speed
+	if(input_direction.length() != 0):
+		pivot.set_rotation(input_direction.angle())
 
 func _physics_process(delta):
 	get_input()
